50eb6b9419b9ec9d84bb2100aa2fb90d60056b6d
Sabriddin <turaevsabriddin53@gmail.com>
2023-12-19 15:44:37 +0500

    123

---
+++ TASK!/Program.cs
@@ -0,0 +1,24 @@
+﻿
+var sabriddin=new Person();
+    System.Console.Write("Enter Your FIRST NAME: ");
+     sabriddin.firstname=Console.ReadLine();
+     System.Console.Write("Enter Your age:");
+     sabriddin.age=Convert.ToInt32(Console.ReadLine());
+      System.Console.Write("Enter Your SECOND NAME:");
+      sabriddin.lastname=Console.ReadLine();;
+
+
+
+
+
+System.Console.Write($"{sabriddin.GETFULLINFO()}");
+
+class Person{
+    public string firstname;
+    public string lastname;
+    public int age;
+    public string GETFULLINFO(){
+       return $"Firstname:{firstname}\nLastname:{lastname}\nAge:{age}";
+    }
+
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,69 @@
+﻿var test = new Post();
+test.SetPost("Hello world");
+test.AddComment("Great");
+for (int i = 0; i < 10; i++)
+{
+    test.AddLike();
+}
+System.Console.WriteLine(test.GETFULLINFO());
+
+
+class Post{
+    public string _text;
+    public int _likes;
+    public string _comments;
+
+    public void SetPost(string text)
+    {
+        _text = text;
+    }
+   public void AddLike(){
+        _likes++;
+   }
+   public void AddComment(string comment)
+   {
+        _comments = comment;
+   }
+   public string GETFULLINFO(){
+     return $"Post text: {_text}, Post comment: {_comments}, Likes: {_likes}";
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+// var pub1=new Person();
+// pub1.text=Console.ReadLine();
+// pub1.Like=Convert.ToInt32(Console.ReadLine());
+
+
+
+// System.Console.WriteLine(pub1.GETFULLINFO());
+
+
+
+// class Person{
+
+//     public string text;
+//     public int Like;
+    
+
+//    public void addlike(int Like){
+//         System.Console.WriteLine(Like++);
+
+//     }
+//     public void newtext(string text){
+//        System.Console.WriteLine(text); ;  
+//     }
+    
+//     public string GETFULLINFO(){
+//        return $"text:{text}\nLike:{Like}\nnewtext:{newtext}\naddlike:{addlike}+";
+//     }
+// }
